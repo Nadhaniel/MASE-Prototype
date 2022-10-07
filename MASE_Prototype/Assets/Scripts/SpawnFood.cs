dcd67c723a26ac63a6a4e51f5279ec096818a201
Nadhaniel <45491434+Nadhaniel@users.noreply.github.com>
2022-10-07 13:19:43 +0100

    Assets and food spawn
    Added assets from opengameart.org and also programmed food spawn

---
+++ MASE_Prototype/Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour
+{
+    public GameObject Food;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+
+    private void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(FoodWave());
+    }
+    private void spawnFood() {
+        GameObject obj = Instantiate(Food) as GameObject;
+        obj.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), Random.Range(-screenBounds.y, screenBounds.y));
+    }
+
+    IEnumerator FoodWave() {
+        while (true) {
+            yield return new WaitForSeconds(respawnTime);
+            spawnFood();
+        }
+        
+    }
+}
